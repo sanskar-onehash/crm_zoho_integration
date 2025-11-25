a148d46bc5f34f83fb6ce0cec897a29ee35079c5
Sanskar Soni <sanskarsoni300@gmail.com>
2025-11-25 14:15:02 +0530

    refactor: remove residuals of api_base_uri
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_zoho_integration/utils.py
+++ crm_zoho_integration/utils.py
@@ -1,5 +1,4 @@
 import frappe
-from frappe import utils
 
 
 def get_zoho_settings(throw=True):
@@ -11,10 +10,5 @@ def get_zoho_settings(throw=True):
     return zoho_settings
 
 
-def get_zoho_base_uri():
-    zoho_settings = get_zoho_settings()
-    return zoho_settings.api_base_uri
-
-
 def get_absolute_url(path):
-    return utils.get_url() + path
+    return frappe.utils.get_url() + path
